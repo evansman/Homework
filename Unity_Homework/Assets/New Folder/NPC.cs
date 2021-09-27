a9113c3b69820187e749beb2db10be8696049b8a
evansman <evan4391234@gmail.com>
2021-09-27 08:52:49 +0800

    npc-homework

---
+++ Unity_Homework/Assets/New Folder/NPC.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    private void Dialogue(string dialogue)
+    {
+
+    }
+    private int UpdateMission(int preMission = 1)
+    {
+        return 0;
+    }
+    private bool MissionFinish(int indexMission)
+    {
+        return false;
+    }
+    public bool openShop()
+    {
+        return true;
+    }
+    public int BuyProp(int price = 100)
+    {
+        return 0;
+    }
+    public void GetMission(int indexMission)
+    {
+
+    }
+}
